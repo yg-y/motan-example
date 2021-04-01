2e3b89636e0352e490b77c20e4adf703e2d9b372
young <y627756022@163.com>
2021-04-01 18:15:39 +0800

    update

---
+++ motan-spring-client/src/main/java/com/young/motan/service/FooService.java
@@ -0,0 +1,11 @@
+package com.young.motan.service;
+
+/**
+ * @author young
+ * @version 1.0
+ * @date 2021/4/1 9:17 上午
+ * @description
+ */
+public interface FooService {
+    String hello(String name);
+}
